c4079e2e6357f09663fb40ca09457f4baea9402b
KohlsAdrian <adriankohls95@gmail.com>
2020-12-20 01:11:59 -0300

    moved config function in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,6 +11,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  void zendesk() async {
+    String accountKey = '';
+    String appId = '';
+
+    String name = '';
+    String email = '';
+    String phoneNumber = '';
+
+    Zendesk2Chat z = Zendesk2Chat.instance;
+
+    await z.init(
+      accountKey,
+      appId,
+      iosThemeColor: Color(0xFFFF5148),
+    );
+
+    await z.customize(
+      departmentFieldStatus: PRE_CHAT_FIELD_STATUS.HIDDEN,
+      emailFieldStatus: PRE_CHAT_FIELD_STATUS.HIDDEN,
+      nameFieldStatus: PRE_CHAT_FIELD_STATUS.HIDDEN,
+      phoneFieldStatus: PRE_CHAT_FIELD_STATUS.HIDDEN,
+      transcriptChatEnabled: true,
+      agentAvailability: false,
+      endChatEnabled: true,
+      offlineForms: true,
+      preChatForm: true,
+      transcript: true,
+    );
+
+    await z.setVisitorInfo(
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+      tags: ['app', 'zendesk2_plugin'],
+    );
+
+    await z.logger(true);
+
+    await z.startChat(
+      toolbarTitle: 'Fale Conosco',
+      backButtonLabel: 'Voltar',
+      botLabel: 'bip bop boting',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,35 +68,7 @@ class _MyAppState extends State<MyApp> {
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.chat),
-          onPressed: () async {
-            String accountKey = '';
-            String appId = '';
-
-            String name = '';
-            String email = '';
-            String phoneNumber = '';
-
-            Zendesk2Chat z = Zendesk2Chat.instance;
-
-            await z.init(
-              accountKey,
-              appId,
-              iosThemeColor: Color(0xFFFF5148),
-            );
-            await z.logger(true);
-            await z.setVisitorInfo(
-              name: name,
-              email: email,
-              phoneNumber: phoneNumber,
-              tags: ['app', 'zendesk2_plugin'],
-            );
-
-            await z.startChat(
-              toolbarTitle: 'Fale Conosco',
-              backButtonLabel: 'Voltar',
-              botLabel: 'bip bop boting',
-            );
-          },
+          onPressed: zendesk,
         ),
       ),
     );
