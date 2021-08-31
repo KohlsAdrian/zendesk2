b8211ad98465b01ee77e593998a538b3e2adf6bf
David Butler <github@nimisis.com>
2021-08-31 12:19:18 +0100

    Bugfix for when no matching agents found
    Error given is "Bad state: No element" when no matching agent is found. Null needs to be returned in this case.

--- example/lib/zendesk_chat.dart
+++ example/lib/zendesk_chat.dart
@@ -6,6 +6,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:zendesk2/chat2/model/provider_model.dart';
 import 'package:zendesk2/zendesk2.dart';
+import 'package:collection/collection.dart';
 
 class ZendeskChat extends StatefulWidget {
   _ZendeskChat createState() => _ZendeskChat();
@@ -274,7 +275,7 @@ class _ZendeskChat extends State<ZendeskChat> {
           Agent? agent;
           if (isAgent)
             agent = _providerModel!.agents
-                .firstWhere((element) => element.displayName == name);
+                .firstWhereOrNull((element) => element.displayName == name);
 
           switch (log.chatLogType.logType) {
             case LOG_TYPE.ATTACHMENT_MESSAGE:
